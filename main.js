ebda544ac0b0de11b84ba9f5916c1195eb2b66ab
AleezaMirza <103499647+AleezaMirza@users.noreply.github.com>
2022-11-21 19:49:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+name_student_array=[];
+
+function submit()
+{
+  
+    var name_1=document.getElementById("name_student_1").value;
+    var name_2=document.getElementById("name_student_2").value;
+    var name_3=document.getElementById("name_student_3").value;
+    var name_4=document.getElementById("name_student_4").value;
+
+    name_student_array.push(name_1);
+    name_student_array.push(name_2);
+    name_student_array.push(name_3);
+    name_student_array.push(name_4);
+
+    console.log(name_student_array);
+
+    document.getElementById("display_name").innerHTML=name_student_array;
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("sort_button").style.display="inline-block";
+
+}
+
+function sort()
+{
+    name_student_array.sort();
+    console.log(name_student_array);
+    document.getElementById("display_name").innerHTML=name_student_array;
+}
